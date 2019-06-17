eaa265c34f6c2bbb1f090090c9fc1db4bd60dff3
Kevin Evangelista <kevin.evangelista11@gmail.com>
2019-06-17 14:33:53 -0500

    structure

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+var reload = require('reload');
+var io = require('sockiet.io')();
+
 
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
@@ -8,13 +11,25 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
 
-
+app.set('views', 'views');
 app.set('view engine', 'ejs');
 
-app.use('/', index)
-app.use('/user', users)
+app.use(express.static("public"));
+app.use(require('./routes/index'));
+app.use(require('./routes/categories'));
+app.use(require('./routes/games'));
+app.use(require('./routes/chatroom'));
+
+app.use('/', index);
+app.use('/user', users);
 
 app.use(cookieParser())
 
-app.listen(3000,)
+io.attach(server);
+io.on('connection', function(socket) {
+    socket.on('postMessage', function(data) {
+        io.emit('updateMessages', data);
+    });
+});
+
 
